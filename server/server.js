7e8febdaa978360329b4230c0d06696d1ba9b34a
Dariusz Legizynski <d.legizynski@.com>
2023-05-15 10:09:43 +0200

    get request functioning

--- server/server.js
+++ server/server.js
@@ -2,21 +2,26 @@ import "dotenv/config.js"
 import express from "express"
 import cors from "cors"
 
-import { pool } from "./db.js"
+import { pool } from "./db/index.js"
 
 const PORT = process.env.PORT ?? 8000
 const app = express()
 
+const API_URL = process.env.API_URL
+
 app.use(cors())
 app.use(express.json())
 
-app.get("/todos/:userEmail", async (req, res) => {
-	const { userEmail } = req.params
+app.get(`${API_URL}`, async (req, res) => {
 
 	try {
-		const getProducts = await pool.query("SELECT * FROM products_description")
-		res.json(getProducts.rows)
+		const getProducts = await pool.query("SELECT * FROM products_description LIMIT 10")
+		res.status(200).json(getProducts.rows)
 	} catch (err) {
 		console.log(err)
 	}
+})
+
+app.listen(PORT, () => {
+  console.log(`server is up an running on port ${PORT}`)
 })
